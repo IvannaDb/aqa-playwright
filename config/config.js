c2e6c0e65660cc0a6120fa4c225026dc13d50ee3
IvannaDb <ivanna.guba@gmail.com>
2024-04-11 15:51:20 +0300

    Added env variables

---
+++ config/config.js
@@ -0,0 +1,12 @@
+
+
+const config = {
+    baseUrl: process.env.BASE_URL,
+    apiUrl: process.env.API_URL,
+    httpCredentials: {
+        username: process.env.HTTP_CREDENTIALS_USERNAME,
+        password: process.env.HTTP_CREDENTIALS_PASSWORD,
+    }
+}
+
+export default config
